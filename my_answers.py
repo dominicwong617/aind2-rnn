ecbd69c520ba77d81edb9a31e93b94305c67b342
Dominic Wong <dominic.wong.617@gmail.com>
2018-05-16 14:53:15 +0000

    Up to 2.4

--- my_answers.py
+++ my_answers.py
@@ -13,7 +13,7 @@ def window_transform_series(series, window_size):
     X = []
     y = []
 
-    for index, ticker in enumerate(series):
+    for index, _ in enumerate(series):
         if index + window_size < len(series):
             X.append(series[index:index+window_size])
             y.append(series[index+window_size])
@@ -36,8 +36,20 @@ def build_part1_RNN(window_size):
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-
-    return text
+    
+    # chars = sorted(list(set(text)))
+    # [' ', '"', '$', '%', '&', "'", '(', ')', '*', '-', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '@', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'à', 'â', 'è', 'é']
+    lowercase_characters = [chr(i) for i in range(97, 97 + 26)]
+    
+    cleaned_text = ''
+    
+    for character in text:
+        if character in punctuation or character in lowercase_characters:
+            cleaned_text = cleaned_text + character
+        else:
+            cleaned_text = cleaned_text + ' '
+            
+    return cleaned_text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
@@ -45,6 +57,10 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
+    for index in range(0, len(text) - window_size, step_size):
+        inputs.append(text[index:index+window_size])
+        outputs.append(text[index+window_size])        
+        
     return inputs,outputs
 
 # TODO build the required RNN model: 
